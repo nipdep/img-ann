a3a7ea41c6d2f6fa4b4aa6329b0b3c1c4a9326b2
nipdep <nipun1deelaka@gmail.com>
2020-12-31 15:29:02 +0530

    Update: convert.py > complete all format converting functions

--- ImgAnn/convert.py
+++ ImgAnn/convert.py
@@ -1,20 +1,21 @@
-# convert method implementation class
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+from .operators.ImgData import ImgData
 from .operators import *
-import os
 import logging
 
-#set logger
+# set logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 class Convertor:
 
-    def __init__(self):
-        pass
+    """ convert method implementation class """
 
-    def coco2csv(self, dataset_dir: str,
+    @staticmethod
+    def coco2csv(dataset_dir: str,
                  coco_ann_dir: str,
                  save_dir: str):
         """ convert coco to csv
@@ -22,20 +23,8 @@ class Convertor:
         :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
         :param coco_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
         :param save_dir: .csv file saving location
-        :return:
+        :return: None
         """
-        if os.path.isfile(save_dir):
-            root, file = os.path.split(save_dir)
-            ext = file.split('.')[-1]
-            if ext == 'csv':
-                logger.error(f"given directory : <{save_dir}> must not include .csv file name.")
-                return
-            else:
-                pass
-        else:
-            logger.error(f"given directory : <{save_dir}> must be a path to a .csv file")
-            return
-
         imgdataset = ImgData.extract(dataset_dir)
         coco_obj = coco.COCO(imgdataset.dataset)
         coco_obj.extract(coco_ann_dir)
@@ -46,10 +35,17 @@ class Convertor:
         csv_fomatted = csv_obj.translate()
         csv_obj.archive(save_dir, csv_fomatted)
 
-    def coco2pascalvoc(self, dataset_dir: str,
-                       coco_ann_dir: str,
-                       save_dir: str):
-        # TODO: convert coco to pascal VOC
+    @staticmethod
+    def coco2voc(dataset_dir: str,
+                 coco_ann_dir: str,
+                 save_dir: str):
+        """ convert coco to pascal VOC
+
+        :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
+        :param coco_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
+        :param save_dir: .csv file saving location
+        :return: None
+        """
         imgdataset = ImgData.extract(dataset_dir)
         coco_obj = coco.COCO(imgdataset.dataset)
         coco_obj.extract(coco_ann_dir)
@@ -57,29 +53,91 @@ class Convertor:
 
         voc_obj = pascalvoc.PascalVOC(imgdataset)
         voc_obj.set_annotations(ann, cls)
+        for xml, name in voc_obj.translate():
+            file_dir = save_dir + '/' + name
+            voc_obj.archive(file_dir, xml)
 
-        pass
-
-    def csv2coco(self, dataset_dir: str,
+    @staticmethod
+    def csv2coco(dataset_dir: str,
                  csv_ann_dir: str,
                  save_dir: str):
-        # TODO: convert .csv into coco
-        pass
+        """ convert .csv into coco
 
-    def csv2pascalvoc(self, dataset_dir: str,
-                      csv_ann_dir: str,
-                      save_dir: str):
-        # TODO: convert .csv into pascal VOC
-        pass
+        :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
+        :param csv_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
+        :param save_dir: .csv file saving location
+        :return: None
+        """
+        imagedataset = ImgData.extract(dataset_dir)
+        csv_obj = csv.CSV(imagedataset)
+        csv_obj.extract(csv_ann_dir)
+        ann, cls = csv_obj.get_annotations()
 
-    def pascalvoc2coco(self, dataset_dir: str,
-                       voc_ann_dir: str,
-                       save_dir: str):
-        # TODO: convert pascal VOC into coco
-        pass
+        coco_obj = coco.COCO(imagedataset)
+        coco_obj.set_annotations(ann, cls)
+        data = coco_obj.translate()
+        coco_obj.archive(save_dir, data)
 
-    def pascalvoc2csv(self, dataset_dir: str,
-                      voc_ann_dir: str,
-                      save_dir: str):
-        # TODO: convert pascal VOC into .csv
-        pass
+    @staticmethod
+    def csv2voc(dataset_dir: str,
+                csv_ann_dir: str,
+                save_dir: str):
+        """ convert .csv into pascal VOC
+
+        :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
+        :param csv_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
+        :param save_dir: .csv file saving location
+        :return: None
+        """
+        imagedataset = ImgData.extract(dataset_dir)
+        csv_obj = csv.CSV(imagedataset)
+        csv_obj.extract(csv_ann_dir)
+        ann, cls = csv_obj.get_annotations()
+
+        voc_obj = pascalvoc.PascalVOC(imagedataset)
+        voc_obj.set_annotations(ann, cls)
+        for xml, name in voc_obj.translate():
+            file_dir = save_dir + '/' + name
+            voc_obj.archive(file_dir, xml)
+
+    @staticmethod
+    def voc2coco(dataset_dir: str,
+                 voc_ann_dir: str,
+                 save_dir: str):
+        """ convert pascal VOC into coco
+
+        :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
+        :param voc_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
+        :param save_dir: .csv file saving location
+        :return: None
+        """
+        imagedataset = ImgData.extract(dataset_dir)
+        voc_obj = pascalvoc.PascalVOC(imagedataset)
+        voc_obj.extract(voc_ann_dir)
+        ann, cls = voc_obj.get_annotations()
+
+        coco_obj = coco.COCO(imagedataset)
+        coco_obj.set_annotations(ann, cls)
+        data = coco_obj.translate()
+        coco_obj.archive(save_dir, data)
+
+    @staticmethod
+    def voc2csv(dataset_dir: str,
+                voc_ann_dir: str,
+                save_dir: str):
+        """ convert pascal VOC into .csv
+
+        :param dataset_dir: relative path current folder, or absolute path to the main folder of the image dataset
+        :param voc_ann_dir: relative path current folder, or absolute path to the main folder of the annotated file
+        :param save_dir: .csv file saving location
+        :return: None
+        """
+        imagedataset = ImgData.extract(dataset_dir)
+        voc_obj = pascalvoc.PascalVOC(imagedataset)
+        voc_obj.extract(voc_ann_dir)
+        ann, cls = voc_obj.get_annotations()
+
+        csv_obj = csv.CSV(imagedataset)
+        csv_obj.set_annotations(ann, cls)
+        csv_fomatted = csv_obj.translate()
+        csv_obj.archive(save_dir, csv_fomatted)
